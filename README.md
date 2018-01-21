667bfe4c3a4d06f73e3c766439e479c7e17a2699
Michael Huang <mh@Michaels-iMac-Pro.local>
2018-01-21 01:41:58 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+countie
+===
+
+Watcher that auto-renames `x.png` to `x_#.png` for quick saving
+
+  npm install -g
+  countie
+
+Add a glob to limit what gets renamed:
+
+  countie *.png
---
+++ index.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+const chokidar = require('chokidar')
+const path = require('path')
+const fs = require('fs')
+
+const glob = process.argv[2] || './*'
+
+const watcher = chokidar.watch(glob, {
+	cwd: '.',
+	ignoreInitial: true,
+	awaitWriteFinish: {
+		stabilityThreshold: 800,
+		pollInterval: 100
+	},
+	atomic: true
+})
+
+watcher
+	.on('ready', () => {
+		console.log(`Watching ${__dirname} for ${glob} ...`)
+	})
+	.on('add', handler)
+	.on('change', handler)
+
+function handler(filePath, stats) {
+	console.log()
+	console.log('Discovered ' + filePath)
+	const ext = path.extname(filePath)
+
+	const notExt = filePath.substr(0,
+		filePath.length - ext.length)
+	const result = /(.*)([0-9]+)$/.exec(notExt)
+
+	if (result) {
+		console.log(`File ${filePath} ends with a number. Exiting.`)
+		return;
+	}
+
+	let newName = null
+	for (let i = 0; !newName; i++) {
+		const tryName = notExt + '_' + i + ext
+		console.log('Seeing if ' + tryName + ' exists...')
+		if (fs.existsSync(tryName)) continue
+		newName = tryName
+	}
+
+	console.log('Renaming ' + filePath + ' to ' + newName + ' ...')
+	fs.renameSync(filePath, newName)
+}
